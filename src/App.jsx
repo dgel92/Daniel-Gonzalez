70763aaba5e1f7855a95c4f21ff16c6341c77635
dgel92 <dgel92@gmail.com>
2022-04-20 23:20:43 -0300

    tercer-desafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.scss';
+import Button from './componentes/Button/button';
 import NavBar from './componentes/NavBar/NavBar';
+import Box from './componentes/Box/Box';
+
 
 function App(){
   return(
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        <Button/>
+        <Box/>
       </header>
     </div>
     );
---
+++ src/componentes/Box/Box.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Box.scss';
+
+
+const Box = () => {
+    return (
+        <div className='box'>Esto es una caja</div>
+    );
+};
+
+export default Box;
